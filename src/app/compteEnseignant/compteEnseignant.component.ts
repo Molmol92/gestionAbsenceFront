cc124a80a81f8640c9cd9a67926e7ecd60d84669
Amal Abdelmaksoud <amal.abdelmaksoud@capgemini.com>
2022-08-30 00:08:42 +0200

    first commit

---
+++ src/app/compteEnseignant/compteEnseignant.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-compteEnseignant',
+    templateUrl: './compteEnseignant.component.html',
+    styleUrls: ['./compteEnseignant.component.css']
+  })
+  export class compteEnseignant implements OnInit {
+  
+    ngOnInit(): void {
+        
+    }
+  }
